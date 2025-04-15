c4dd5801edf4daf0547b42b455e3fb00c9c191a9
shubh-codes7 <shubhamcodes7@gmail.com>
2025-04-15 08:07:17 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+// createRoot(document.querySelector("#root")).render(<App/>)
 
-
+ReactDOM.render(<App/>, document.querySelector("#root"))
